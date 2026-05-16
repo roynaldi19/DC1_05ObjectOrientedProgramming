ef602697267711aa84bbf51ef009f5cc8bf5f5e0
roynaldi19 <roynaldi19@gmail.com>
2026-05-16 08:52:21 +0700

    uodaate mei 26

---
+++ src/main/kotlin/01Class/Animal.kt
@@ -0,0 +1,16 @@
+package `01Class`
+
+class Animal() {
+    var name: String = "Kucing"
+    var weight: Double = 3.2
+    var age: Int = 2
+    var isMammal: Boolean = true
+
+    fun eat(){
+        println("$name makan!")
+    }
+
+    fun sleep() {
+        println("$name tidur!")
+    }
+}
---
+++ src/main/kotlin/01Class/Main.kt
@@ -0,0 +1,7 @@
+package `01Class`
+
+fun main() {
+    val Cat1 = Animal()
+
+    println("Nama: ${Cat1.name}, Berat:")
+}
